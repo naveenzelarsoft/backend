8647682711bf2a1735fb6f0d20658b9bd7819662
naveen <naveen@gmail.com>
2021-09-17 23:00:55 +0530

    env

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 const env = require('./env.js');
  
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('${env.database.PGDATABASE}', '${env.username.PGUSER}', '${env.password.PGPASSWORD}', {
-  host: env.host.DB_HOST,
+const sequelize = new Sequelize('${env.database}', '${env.username}', '${env.password}', {
+  host: env.host,
   dialect: env.dialect,
   operatorsAliases: false,
  
