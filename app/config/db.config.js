827d475c8808300bfa63bd0be98ea50bf6a270d8
naveen <naveen@gmail.com>
2021-09-18 17:42:04 +0530

    env

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -13,7 +13,12 @@ const sequelize = new Sequelize(env.database, env.username, env.password, {
     idle: env.pool.idle
   }
 });
-const db = {};
+const db = {
+    username: process.env.DB_USERNAME ,
+    password: process.env.DB_PASSWORD ,
+    host: process.env.DB_HOST ,
+    database: process.env.DB_NAME
+};
  
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
