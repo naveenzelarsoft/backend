1deca833bb658f95cd8708c837e073701d9a5955
naveen <naveen@gmail.com>
2021-09-18 10:59:13 +0530

    env

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,7 +1,7 @@
 const env = require('./env.js');
  
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(env.database, env.username, env.password, {
+const sequelize = new Sequelize(proces.env.database, process.env.username, process.env.password, {
   host: env.host,
   dialect: env.dialect,
   operatorsAliases: false,
