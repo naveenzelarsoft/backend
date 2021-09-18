e18892e2c5a3b4fd6d10edb6cd09145f67995aaf
naveen <naveen@gmail.com>
2021-09-18 18:33:19 +0530

    env

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -3,7 +3,7 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.database, process.env.username, process.env.password, {
   host: process.env.host,
-  dialect: postgres,
+  dialect: process.env.dialect,
   operatorsAliases: false,
  
   pool: {
